47677cf0c8f9e4d210f701090289900b16efcf4a
xz-dev <xiangzhedev@gmail.com>
2024-11-09 13:56:31 +0800

    sys-kernel/cachyos-sources: update kernel-build-zfs.sh
    Signed-off-by: xz-dev <xiangzhedev@gmail.com>

--- sys-kernel/cachyos-sources/files/kernel-build-zsh.sh
+++ sys-kernel/cachyos-sources/files/kernel-build-zsh.sh
@@ -12,6 +12,12 @@ set -e
 
 KERNEL_ROOT="/usr/src/linux"
 #MAKEOPTS="LLVM=1 LLVM_IAS=1 CC=clang LD=ld.lld"
+# make function with args
+kernel_make() {
+    #make -j$(( $(nproc) + 1 )) ${MAKEOPTS} KCFLAGS="-O3 -march=native -pipe"
+    make -j$(( $(nproc) + 1 )) ${MAKEOPTS} KCFLAGS="-pipe" $@
+}
+
 
 cd $KERNEL_ROOT
 #if [ -d ./zfs ]; then
@@ -28,16 +34,10 @@ cd $KERNEL_ROOT
 #    ./scripts/config -e CONFIG_ZFS
 #fi
 #make clean
-make ${MAKEOPTS} olddefconfig
+kernel_make olddefconfig
 
-# make function with args
-build() {
-    #make -j$(( $(nproc) + 1 )) ${MAKEOPTS} KCFLAGS="-O3 -march=native -pipe"
-    make -j$(( $(nproc) + 1 )) ${MAKEOPTS} KCFLAGS="-pipe" $@
-}
-
-build all
-make -C tools/bpf/bpftool vmlinux.h feature-clang-bpf-co-re=1
+kernel_make all
+kernel_make -C tools/bpf/bpftool vmlinux.h feature-clang-bpf-co-re=1
 #make modules_prepare
 
 # check zfs dir exists
@@ -50,11 +50,12 @@ if [ -d ./zfs ]; then
         --libdir=/usr/lib --datadir=/usr/share --includedir=/usr/include \
         --with-udevdir=/lib/udev --libexecdir=/usr/lib/zfs --with-config=kernel \
         --with-linux=$KERNEL_ROOT
-    build
-    make install; ldconfig; depmod
+    kernel_make
+    kernel_make install;
+    #ldconfig; depmod
     cd ..
 fi
 
-make modules_install
-make install
+kernel_make modules_install
+kernel_make install
 emerge @module-rebuild

d9a50146e95269bcf830b2af2bffaa552dbd3b45
xz-dev <xiangzhedev@gmail.com>
2024-12-19 20:28:06 +0800

    sys-kernel/cachyos-sources: ^^ ( zfs kcfi )
    https://github.com/openzfs/zfs/issues/15911
    
    Signed-off-by: xz-dev <xiangzhedev@gmail.com>

--- sys-kernel/cachyos-sources/files/kernel-build-zsh.sh
+++ sys-kernel/cachyos-sources/files/kernel-build-zsh.sh
@@ -5,13 +5,11 @@ set -e
 . /etc/profile
 #modprobe dm-crypt
 
-#export CCACHE_DIR="/var/cache/ccache/kernel"
-#export KERNEL_CC="ccache clang" UTILS_CC="ccache clang" UTILS_CXX="ccache clang++"
-#export KERNEL_CC="ccache gcc" UTILS_CC="ccache gcc" UTILS_CXX="ccache g++"
-#export KERNEL_CC="clang" UTILS_CC="clang" UTILS_CXX="clang++"
+#export KERNEL_CC="gcc" UTILS_CC="gcc" UTILS_CXX="g++"
+export KERNEL_CC="clang" UTILS_CC="clang" UTILS_CXX="clang++"
 
 KERNEL_ROOT="/usr/src/linux"
-#MAKEOPTS="LLVM=1 LLVM_IAS=1 CC=clang LD=ld.lld"
+MAKEOPTS="LLVM=1 LLVM_IAS=1 CC=clang LD=ld.lld"
 # make function with args
 kernel_make() {
     #make -j$(( $(nproc) + 1 )) ${MAKEOPTS} KCFLAGS="-O3 -march=native -pipe"
@@ -20,19 +18,6 @@ kernel_make() {
 
 
 cd $KERNEL_ROOT
-#if [ -d ./zfs ]; then
-#    cd ./zfs
-#    ./autogen.sh
-#    CONFIGURE_FLAGS=()
-#    CONFIGURE_FLAGS+=("KERNEL_LLVM=1")
-#    ./configure "${CONFIGURE_FLAGS[@]}" --prefix=/usr --sysconfdir=/etc --sbindir=/usr/bin \
-#        --libdir=/usr/lib --datadir=/usr/share --includedir=/usr/include \
-#        --with-udevdir=/lib/udev --libexecdir=/usr/lib/zfs --with-config=kernel \
-#        --with-linux=$KERNEL_ROOT --enable-linux-builtin 
-#    ./copy-builtin $KERNEL_ROOT
-#    cd ..
-#    ./scripts/config -e CONFIG_ZFS
-#fi
 #make clean
 kernel_make olddefconfig
 
@@ -44,7 +29,7 @@ kernel_make -C tools/bpf/bpftool vmlinux.h feature-clang-bpf-co-re=1
 if [ -d ./zfs ]; then
     cd ./zfs
     CONFIGURE_FLAGS=()
-    #CONFIGURE_FLAGS+=("KERNEL_LLVM=1")
+    CONFIGURE_FLAGS+=("KERNEL_LLVM=1")
     ./autogen.sh
     ./configure "${CONFIGURE_FLAGS[@]}" --prefix=/usr --sysconfdir=/etc --sbindir=/usr/bin \
         --libdir=/usr/lib --datadir=/usr/share --includedir=/usr/include \
